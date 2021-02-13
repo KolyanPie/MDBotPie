ea08b02370ad93ac58211ec17fefd0de71c27712
KolyanPie <mbilibin@mail.ru>
2021-02-13 14:40:25 +0300

    Initial commit

---
+++ src/main/java/ru/kolyanpie/MDBotPieApplication.java
@@ -0,0 +1,12 @@
+package ru.kolyanpie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MDBotPieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MDBotPieApplication.class, args);
+    }
+}
